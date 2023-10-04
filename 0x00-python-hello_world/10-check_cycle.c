3f60b5fd9dfe15265ba19424d460f56b7abe6703
Solomon <solomonchamamme@gmail.com>
2023-10-04 21:21:02 +0000

    Created the checking file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ *check_cycle - a function to check if ther is a cycle in a linked list
+ *@list: The linked list to be checked
+ *Return: 0 on not finding a cycle
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *node;
+listint_t *node_2;
+
+node = list;
+node_2 = node;
+if (node == NULL || node_2 == NULL)
+	return (0);
+
+while (node_2 != NULL)
+{
+
+node_2 = node_2->next->next;
+node = node->next;
+	if (node == node_2)
+	{
+		return (1);
+	}
+}
+return (0);
+}
