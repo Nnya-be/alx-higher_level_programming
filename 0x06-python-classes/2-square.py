c5856699e3be3ed1f3a22f306fd6b5911fb580c6
Solomon <solomonchamamme@gmail.com>
2023-10-25 03:42:29 +0000

    Made corrections for 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -6,18 +6,19 @@ class Square:
     """A Class Implementation of a square."""
 
     def __init__(self, size=0):
-        """Initialize function for the class square.
+        """Initialize constructor function for the class square.
 
-        Args: size of type int and returns nothing.
+        Args:
+             size(int): The size of the square. I must be non-negative.
+
+        Raises:
+              TypeError: If the size is not an integer.
+              ValueError: If the size is negative.
         """
         try:
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
             if (size < 0):
-                raise ValueError
-            if (not isinstance(size, int)):
-                raise TypeError
-
+                raise ValueError("size must be >= 0")
+        finally:
             self.__size = size
-        except TypeError:
-            print("size must be an integer")
-        except ValueError:
-            print("size must be >= 0")
