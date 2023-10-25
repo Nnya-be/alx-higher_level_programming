83a527b242b4edc1638cb85d287c04c590c05bcf
Solomon <solomonchamamme@gmail.com>
2023-10-25 01:18:26 +0000

    Created the second square file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This is another square definition extending the 0-square."""
+
+
+class Square:
+    """The size privated attribute."""
+
+    def __init__(self, size):
+        """Args: size: first argument."""
+        self._size = size
