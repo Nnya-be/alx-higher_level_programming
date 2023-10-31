2ffeb214ea42f086827326e2db30358f16895630
Solomon <solomonchamamme@gmail.com>
2023-10-31 11:43:29 +0000

    Checked: pycodestyle and pydocstyle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -85,5 +85,4 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         elif type(value) is not int:
             raise TypeError("height must be an integer")
-
         self.__height = value
