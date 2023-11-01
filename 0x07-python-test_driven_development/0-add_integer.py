1798fa50ebef74ae05e8fd2b97d799f9d431ca32
Solomon <solomonchamamme@gmail.com>
2023-11-01 08:11:54 +0000

    Modified: Add-integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Adding integers"""
+"""Adding integers."""
 
 
 def add_integer(a, b=98):
-    """ Check if a and b are integers or floats.
-    
+    """Check if a and b are integers or floats.
+
     It also has test cases in the other files.
     """
-    
     if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
     a = int(a) if isinstance(a, float) else a
