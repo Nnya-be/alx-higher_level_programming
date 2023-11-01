38c4a020b971f482079a850bd5c507e1badf0ab8
Solomon <solomonchamamme@gmail.com>
2023-11-01 10:05:22 +0000

    Added most cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,9 +7,16 @@ def add_integer(a, b=98):
 
     It also has test cases in the other files.
     """
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
-    a = int(a) if isinstance(a, float) else a
-    b = int(b) if isinstance(b, float) else b
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    elif a is True or a is False:
+        raise TypeError("a must be an integer")
+    elif b is True or b is False:
+        raise TypeError("b must be an integer")
+    else:
+        a = int(a) if isinstance(a, float) else a
+        b = int(b) if isinstance(b, float) else b
     """Returns the statement"""
     return a + b
