ea05a1c9e20a41b8924a7966d52a5d805f215758
Solomon <solomonchamamme@gmail.com>
2023-11-08 04:07:40 +0000

    Modified: 0.Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,17 +10,10 @@ def read_file(filename=""):
 
     Args:
         filename(str): Name of the file to read.
-    
+
     Returns:
         None
-
-    Raises:
-        IOError: If there exits an issue reading the file.
     """
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                print(line.strip())
-    except IOError:
-        raise IOError(f"Error reading file '{filename}':")
-        
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line.strip())
