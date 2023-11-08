73c43e96f9323ff9597ac4f7c3c344f2d8ec2dc3
Solomon <solomonchamamme@gmail.com>
2023-11-08 01:47:53 +0000

    Modified: The task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-"""Function to read a text file and print it to the stdout
-in the UTF-8 format.
+"""Function to read a text file.
+
+and print it to the stdout in the UTF-8 format.
 """
 
+
 def read_file(filename=""):
+    """Read The files using a loop.
+
+    Args:
+    filename: initialized to an empty string.
+    Return:None
+    """
+    if filename is None:
+        raise TypeError("No file name given:")
     with open(filename) as file:
         for lines in file:
             print(lines.strip())
