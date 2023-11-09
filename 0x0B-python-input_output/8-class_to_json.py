4d07004936439c5533b5f76cf80a4b45ad3edf53
Solomon <solomonchamamme@gmail.com>
2023-11-09 04:51:01 +0000

    Created: 8.Class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """Return a dictionary description with simple data structure."""
-import json
 
 
 def class_to_json(obj):
     """Json converter of objects."""
-    return json.dumps(obj.__dict__)
+    return obj.__dict__
