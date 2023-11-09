c63464373a8f5c8549dc4061baa7aa889330fd49
Solomon <solomonchamamme@gmail.com>
2023-11-09 05:09:13 +0000

    Created: 10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Student Module"""
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student object with first_name, last_name, and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of the Student instance."""
+        if attrs is None:
+            return self.__dict__
+
+        student_dict = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                student_dict[attr] = getattr(self, attr)
+        return student_dict
