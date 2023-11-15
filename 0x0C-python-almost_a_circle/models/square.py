e92b973e89fefba6ee9565a20bffe4fcf6302c44
Solomon <solomonchamamme@gmail.com>
2023-11-15 23:13:18 +0000

    Modified: setting

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,6 +17,17 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Representation String of the Square."""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
