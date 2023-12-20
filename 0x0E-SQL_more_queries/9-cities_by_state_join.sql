595428c6eb699a839c54c7c219e81c37eef6c416
Solomon <solomonchamamme@gmail.com>
2023-12-20 23:28:46 +0000

    Modified: task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Select all cities with corresponding state names
-SELECT cities.id, cities.name, states.name AS state_name
+SELECT DISTINCT cities.id, cities.name, states.name
 FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+JOIN (states) ON cities.state_id = states.id
+ORDER BY id ASC;
