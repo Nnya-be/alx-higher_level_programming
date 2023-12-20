0de71a6ba3b6e93c7eee9392e4f800f7bf2d05c9
Solomon <solomonchamamme@gmail.com>
2023-12-20 20:00:49 +0000

    Modified: Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Creates MySQL server user and grants privileges.
 CREATE USER 'user_0d_1'@'localhost'IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
-GRANT PRIVILEGE ON *.* TO 'user_0d_1';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1';
