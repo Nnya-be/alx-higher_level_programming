5c3052ef0d4d7de03d4c4861145f6391c6a397a5
Solomon <solomonchamamme@gmail.com>
2023-12-20 23:15:03 +0000

    Created: file 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,4 @@
 -- Create the table unique_id on the server.
 CREATE TABLE IF NOT EXISTS unique_id (
        id INT DEFAULT 1 UNIQUE,
-       name VARCHAR(256),
-       );
+       name VARCHAR(256));
