0772bd3635946e645d7440768e866031a9927c1c
Solomon <solomonchamamme@gmail.com>
2023-12-20 23:07:00 +0000

    Modified: task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,4 +1,4 @@
---Creates the table id_not_null on the server.
+-- Creates the table id_not_null on the server.
 CREATE TABLE IF NOT EXISTS id_not_null(
        id INT DEFAULT 1,
        name VARCHAR(256)
