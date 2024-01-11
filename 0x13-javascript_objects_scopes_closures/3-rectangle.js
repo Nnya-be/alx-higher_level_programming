efc46905e2ca655c39d30f7e8df544f952d648e9
Solomon <solomonchamamme@gmail.com>
2024-01-11 22:55:51 +0000

    Fixed: semistandard errors

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,15 +5,16 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
-   print() {
-	for (let i = 0; i < this.height; i++) {
-	    let shape = '';
-	    for (let j = 0; j < this.width; j++) {
-		shape += 'X';
-	    }
-	    console.log(shape);
-	}
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let shape = '';
+      for (let j = 0; j < this.width; j++) {
+        shape += 'X';
+      }
+      console.log(shape);
     }
+  }
 }
 
 module.exports = Rectangle;
