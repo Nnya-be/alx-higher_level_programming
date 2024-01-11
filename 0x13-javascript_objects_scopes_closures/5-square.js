b28db6bf99ffcde7ebfabf6b046c1ddf43c11c2a
Solomon <solomonchamamme@gmail.com>
2024-01-11 23:50:52 +0000

    Semistandard errors fixed

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,9 +2,8 @@
 const Rectangle = require('./4-rectangle.js');
 
 class Square extends Rectangle {
-
   constructor (size) {
-    super (size, size);
+    super(size, size);
   }
 }
 module.exports = Square;
