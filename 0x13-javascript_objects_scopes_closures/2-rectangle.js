57b03856178137b2e32c132c37cf313877c0d471
Solomon <solomonchamamme@gmail.com>
2024-01-11 22:44:36 +0000

    Modifified: for uninitialized arguments

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0) { return this; }
+  constructor (w = 0, h = 0) {
+    if (w < 1 || h < 1) { return this; }
     this.width = w;
     this.height = h;
   }
