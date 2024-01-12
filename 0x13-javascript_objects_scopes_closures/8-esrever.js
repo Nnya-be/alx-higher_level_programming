87815077e86bc638615b262b50ddf1ce19a44c62
Solomon <solomonchamamme@gmail.com>
2024-01-12 23:25:57 +0000

    Fixed: semistandard error

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let reversedList = [];
+  const reversedList = [];
 
   for (let a = list.length - 1; a >= 0; a--) {
     reversedList.push(list[a]);
   }
   return reversedList;
-}
+};
