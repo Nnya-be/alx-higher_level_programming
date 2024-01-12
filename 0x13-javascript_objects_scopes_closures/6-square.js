b25ac019660025451a3a8f8be5e6fe46887f3032
Solomon <solomonchamamme@gmail.com>
2024-01-12 22:41:19 +0000

    Modified

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
-class Square extends require('./5-square.js') {
+class Square extends require('./5-square.js')
+{
   constructor(size) {
     super(size);
   }
+
   charPrint(c) {
     if (c === undefined) {
       c = 'X';
     }
+    
     for (let i = 0; i < this.size; i++) {
-        console.log(c.repeat(this.size));
-      }
+      console.log("h");
+    }
   }
 }
 module.exports = Square;
