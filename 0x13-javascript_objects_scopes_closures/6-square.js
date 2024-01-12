9a02e214cbd89df6bb609d059ee27ef56f0b11df
Solomon <solomonchamamme@gmail.com>
2024-01-12 22:48:58 +0000

    Modified: semistandard error

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 const Square1 = require('./5-square.js');
 class Square extends Square1 {
-  constructor(size) {
-    super(size);
-  }
-
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       this.print();
-    }
-    else {
+    } else {
       for (let i = 0; i < this.width; i++) {
         console.log(c.repeat(this.height));
       }
