fe6cb7ada007f58625e3eacaf0aa3057a87c781f
Solomon <solomonchamamme@gmail.com>
2024-01-14 22:50:49 +0000

    Modified

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""Script to list all states from a database."""
-import sys
+"""Select all states in the databse."""
 import MySQLdb
-
-def list_states(username, password, database):
-    connection = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-
-    cursor = connection.cursor()
-
-    query = "SELECT * FROM states ORDER BY states.id ASC"
-    cursor.execute(query)
-
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-
-    cursor.close()
-    connection.close()
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <mysql_username> <mysql_password> <database_name>")
-        sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    list_states(username, password, database)
+    username = sys.argv[1]
+    pas = sys.argv[2]
+    dbn = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=pas, db=dbn)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY states.id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
