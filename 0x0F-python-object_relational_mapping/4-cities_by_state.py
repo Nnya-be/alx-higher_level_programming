c58fa45ddd19395e82cd172a18431705c8c46223
Solomon <solomonchamamme@gmail.com>
2024-01-15 21:38:07 +0000

    Created: cities by city

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,12 +9,13 @@ if __name__ == "__main__":
     username = sys.argv[1]
     pas = sys.argv[2]
     dbn = sys.argv[3]
-    statex = sys.argv[4]
+
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=username, passwd=pas, db=dbn)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name like %s\
-                             ORDER BY states.id", (statex,))
+    cur.execute("""SELECT cities.id, cities.name, states.name
+                FROM states join cities
+                on cities.state_id = states.id ORDER BY cities.id""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
