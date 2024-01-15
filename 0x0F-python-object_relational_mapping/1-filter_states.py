0c188387d19c376d66816bb59759c8f955063cc0
Solomon <solomonchamamme@gmail.com>
2024-01-15 19:17:58 +0000

    Modified

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """Module to get all state starting with N and sort them."""
 import sys
 import MySQLdb
@@ -5,11 +6,12 @@ import MySQLdb
 if __name__ == '__main__':
     if (sys.argv != 4):
         return
-    usr,pas,dab = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
-    db = MySQLdb.connect(host='localhost',port=3306,user=usr,passwd=pas,db=dab)
+    usr, pas, dab = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+    db = MySQLdb.connect(host='localhost', port=3306, user=usr,
+                         passwd=pas, db=dab)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states
-    WHERE name LIKE 'N%' ORDER BY state.id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'
+                ORDER BY state.id ASC")
     state = cur.fetchall()
     for state in states:
         print(state)
