d88857e16baf2392945d17d6c7a1e7129ecd366c
Solomon <solomonchamamme@gmail.com>
2024-01-16 09:49:03 +0000

    Crreated: the model_state fetch first file

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Prints the first State object from the hbtn_0e_6_usa db."""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    usr, pas, db = argv[1], argv[2], argv[3]
+    engine = create_engine(f"mysql+mysqldb://{usr}:{pas}@localhost:3306/{db}",
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    first_state = session.query(State).order_by(State.id).first()
+    if first_state:
+        print(f"{first_state.id}, first_state.name")
+    else:
+        print("Nothin")
+
+    session.close()
