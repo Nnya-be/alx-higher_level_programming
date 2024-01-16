a1619906862127b07e60b43ad26ea6a8e5a70f1b
Solomon <solomonchamamme@gmail.com>
2024-01-16 10:06:00 +0000

    Created: the fileter for states with a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lists all State objecgts that contain letter a from hbtn_0e_6_usa db."""
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+
+if __name__ == '__main__':
+    usr, pas, db = argv[1], argv[2], argv[3]
+    engine = create_engine(f"mysql+mysqldb://{usr}:{pas}@localhost:3306/{db}",
+                           poop_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states_a = session.query(State).filter(State.name.like('%a%')).
+    order_by(State.id).all()
+    for state in state_a:
+        print(f"{state.id}: {state.name}")
+
+        session.close()
