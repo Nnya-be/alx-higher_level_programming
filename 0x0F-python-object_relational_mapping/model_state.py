60c7d0c130748293bd1965c0eb4b426021587c0c
Solomon <solomonchamamme@gmail.com>
2024-01-16 09:22:00 +0000

    Pycodestyles check

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -16,7 +16,7 @@ Base = declarative_base()
 class State(Base):
     """State Representations."""
     __tablename__ = 'states'
-    
+
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True)
     name = Column(String(128), nullable=False)
