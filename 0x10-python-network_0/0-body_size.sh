2a0c3ca3550dfa813c648b38be157a6c3a519dc1
Solomon <solomonchamamme@gmail.com>
2024-01-26 04:40:38 +0000

    MOdified

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 #Writes the content of a request lenght.
 
-url=$1
-size=$(curl -sI "$url" | grep -i "Content-length' | awk '{print $2}' | tr -d '\r\n')
-echo "$size"
+curl -sI "$1" | grep Content-Length | cut -d' ' -f2
