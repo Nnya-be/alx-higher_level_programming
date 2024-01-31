8e224c4021eb1efa2ccfd68e8b126ac4793fe71c
Solomon <solomonchamamme@gmail.com>
2024-01-31 05:21:52 +0000

    Modified

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,16 @@
 """
 Python script that fetches the intranet url.
 """
-import urllib.request
+from urllib import request
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(the_page)))
-    print("\t- content: {}".format(the_page))
-    print("\t- utf8 content: {}".format(the_page.decode()))
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    try:
+        with request.urlopen(url) as response:
+            content = response.read().decode('utf-8')
+
+            print(f"Body response:")
+            print(f"\t- type: {type(content)}")
+            print(f"\t- content: {content}")
+    except Exception as e:
+        print(f"Error: {e}")
