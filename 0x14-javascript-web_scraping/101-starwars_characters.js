38fdbfc2f1926e96ce3a0cf6bc9e22abcc682624
Solomon <solomonchamamme@gmail.com>
2024-02-21 02:37:37 +0000

    Created: the last piece

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,42 +2,27 @@
 
 const request = require('request');
 const endpoint = 'https://swapi-api.hbtn.io/api/films/';
-const movieId = process.argv[2];
-const url = `${endpoint}${movieId}`;
-const characterUrls = [];
-const characterNames = [];
+const filter = process.argv[2];
+const url = endpoint + filter;
+const listx = [];
+const listname = [];
 let count = 0;
-
-function printCharacterNames () {
-  for (let x = 0; x < characterNames.length; x++) {
-    console.log(characterNames[x]);
-  }
-}
-
-function fetchCharacterDetails (characterUrl) {
-  request(characterUrl, function (error, response, body) {
-    if (error) {
-      return console.log(error);
-    }
-
-    characterNames.push(JSON.parse(body).name);
-    count++;
-
-    if (count === characterUrls.length) {
-      printCharacterNames();
-    }
-  });
-}
-
 request(url, function (error, response, body) {
-  if (error) {
-    return console.log(error);
+  if (error) { return console.log(error); } else {
+    for (let m = 0; m < JSON.parse(body).characters.length; m++) {
+      listx.push(JSON.parse(body).characters[m]);
+    }
   }
-
-  const characters = JSON.parse(body).characters;
-
-  for (let m = 0; m < characters.length; m++) {
-    characterUrls.push(characters[m]);
-    fetchCharacterDetails(characters[m]);
+  for (let k = 0; k < listx.length; k++) {
+    request(listx[k], function (e, r, b) {
+      if (e) { return console.log(e); }
+      listname.push(JSON.parse(b).name);
+      count++;
+      if (count === listx.length) {
+        for (let x = 0; x < listname.length; x++) { console.log(listname[x]); }
+      }
+   
+    });
+   
   }
 });
