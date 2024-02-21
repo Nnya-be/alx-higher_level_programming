1beaae37bbc92fad8f0ec599071c4bdbeb7f9792
Solomon <solomonchamamme@gmail.com>
2024-02-21 02:24:01 +0000

    Modified

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,33 @@
 #!/usr/bin/node
 
 const request = require('request');
-const ar = [];
-const url = process.argv[2];
-request(url, function (err, response, body) {
-  if (err) { console.log(err); } else if (response.statusCode === 200) {
-    let count = 0;
-    for (const c of JSON.parse(body).results) {
-      ar.push(c.characters);
-    }
-    for (let i = 0, len = ar.length; i < len; i++) {
-      for (let j = 0, len2 = ar[i].length; j < len2; j++) {
-        if (ar[i][j] === 'https://swapi-api.hbtn.io/api/people/18/') { count++; }
+
+const apiUrl = process.argv[2];
+
+function countMoviesWithCharacter(apiUrl, characterId) {
+  return new Promise((resolve, reject) => {
+    request(apiUrl, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode === 200) {
+        const movies = JSON.parse(body).results;
+        const count = movies.reduce((acc, movie) => {
+          const characters = movie.characters.map(character => character.split('/').slice(-2, -1)[0]);
+          if (characters.includes(characterId)) {
+            return acc + 1;
+          }
+          return acc;
+        }, 0);
+        resolve(count);
+      } else {
+        reject('Invalid API response');
       }
-    }
-    console.log(count);
-  } else { console.log(''); }
-});
+    });
+  });
+}
+
+const characterId = '18';
+
+countMoviesWithCharacter(apiUrl, characterId)
+  .then(count => console.log(count))
+  .catch(error => console.error(error));
